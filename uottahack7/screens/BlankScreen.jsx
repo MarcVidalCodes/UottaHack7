7b953ede5c797e1c297efc70a1d4cbfc61531316
Benjamin Wu <bwu062005@gmail.com>
2025-01-18 00:30:05 -0500

    Update BlankScreen.jsx
    Created a blank screen template for future use. All text aligned to the center.

---
+++ uottahack7/screens/BlankScreen.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+const BlankScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is a blank screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
+export default BlankScreen;
